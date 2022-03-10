ea567c1b4736f053db072df158923a9a4d26dcb7
Luca Ferrario <ferrariolucait@gmail.com>
2022-03-10 11:22:22 +0100

    template

--- src/App.js
+++ src/App.js
@@ -4,18 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>works</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar"></div>
+      <div className="maincontainer"></div>
     </div>
   );
 }
