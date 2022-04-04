906b7025b07d3319dfae76b832f3c724c6e16e8b
Luca Ferrario <ferrariolucait@gmail.com>
2022-04-04 18:12:28 +0200

    remove bug code

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,35 +15,6 @@ function Home() {
   const { signout } = useAuth();
   const navigate = useNavigate();
 
-  const [chart1Data, setChart1Data] = useState({
-    labels: ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"],
-    datasets: [
-      {
-        label: "My First Dataset",
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(201, 203, 207, 0.2)",
-        ],
-        borderColor: [
-          "rgb(255, 99, 132)",
-          "rgb(255, 159, 64)",
-          "rgb(255, 205, 86)",
-          "rgb(75, 192, 192)",
-          "rgb(54, 162, 235)",
-          "rgb(153, 102, 255)",
-          "rgb(201, 203, 207)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  });
-
   function handleSignout() {
     setError("");
 
@@ -109,9 +80,7 @@ function Home() {
         </div>
         <div className="maincontainer">
           <h1 className="containerheader">Good morning, {name}</h1>
-          <div className="mainpanel1">
-            <BarChart chartData={chart1Data} />
-          </div>
+          <div className="mainpanel1">{/* <BarChart chartData={} /> */}</div>
           <div className="mainpanelscont">
             <div className="bottompanel">
               <h1 className="bottomh">Which Habits have you respected?</h1>
