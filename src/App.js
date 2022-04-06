4c8408e2ff494484cd059d7530507a8d5bb928ef
Luca Ferrario <ferrariolucait@gmail.com>
2022-04-06 18:42:13 +0200

    new screens folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomeScreen from "./screens1/HomeScreen";
-import RegisterScreen from "./screens1/auth/RegisterScreen";
-import LoginScreen from "./screens1/auth/LoginScreen";
-import Page2Screen from "./screens1/Page2Screen.js";
-import Page3Screen from "./screens1/Page3Screen.js";
+import HomeScreen from "./screens/HomeScreen";
+import RegisterScreen from "./screens/auth/RegisterScreen";
+import LoginScreen from "./screens/auth/LoginScreen";
+import Page2Screen from "./screens/Page2Screen.js";
+import Page3Screen from "./screens/Page3Screen.js";
 
 function App() {
   return (
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Home from "../components/Home";
+import { AuthProvider } from "../contexts/AuthContext";
 
+const HomeScreen = () => {
+  return (
+    <AuthProvider>
+      <Home />
+    </AuthProvider>
+  );
+};
+
+export default HomeScreen;
