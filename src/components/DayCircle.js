da8e2ae626d0876934fd1a1c4fa54af710f354ce
Luca Ferrario <ferrariolucait@gmail.com>
2022-04-08 08:44:10 +0200

    colors component

--- src/components/DayCircle.js
+++ src/components/DayCircle.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { HomeData } from "../Charts Data/HomeData";
 
 function DayCircle({ index }) {
-  return <div className={"quality" + HomeData[index].color.toString}></div>;
+  return <div className={`quality-${HomeData[index].color}`}></div>;
 }
 
 export default DayCircle;
