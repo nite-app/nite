9772cd0b4ca2e581b21cb9b0e70e27ffbd0addae
Luca Ferrario <ferrariolucait@gmail.com>
2022-04-08 08:45:05 +0200

    colors

--- src/Charts Data/HomeData.js
+++ src/Charts Data/HomeData.js
@@ -9,30 +9,36 @@ export const HomeData = [
     id: 1,
     day: "TUE",
     sleep: 6.5,
+    color: "red",
   },
   {
     id: 2,
     day: "WED",
     sleep: 7,
+    color: "yellow",
   },
   {
     id: 3,
     day: "THU",
     sleep: 7,
+    color: "yellow",
   },
   {
     id: 4,
     day: "FRI",
     sleep: 7.5,
+    color: "green",
   },
   {
     id: 5,
     day: "SAT",
     sleep: 6,
+    color: "red",
   },
   {
     id: 6,
     day: "SUN",
     sleep: 9,
+    color: "green",
   },
 ];
