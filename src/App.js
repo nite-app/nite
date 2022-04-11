9633022fcd823108cb6e60802e7b8f52dbe133d7
Luca Ferrario <ferrariolucait@gmail.com>
2022-04-11 17:21:33 +0200

    FANCULOMB FUNZIONA

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 
 import HomeScreen from "./screens/HomeScreen";
 import RegisterScreen from "./screens/auth/RegisterScreen";
@@ -13,16 +14,18 @@ import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<PrivateRoute />}>
-          <Route exact path="/" element={<HomeScreen />} />
-          <Route exact path="/page2" element={<Page2Screen />} />
-          <Route exact path="/page3" element={<Page3Screen />} />
-        </Route>
-        <Route exact path="/login" element={<LoginScreen />} />
-        <Route exact path="/register" element={<RegisterScreen />} />
-        <Route exact path="/reset" element={<ResetScreen />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route element={<PrivateRoute />}>
+            <Route exact path="/" element={<HomeScreen />} />
+            <Route exact path="/page2" element={<Page2Screen />} />
+            <Route exact path="/page3" element={<Page3Screen />} />
+          </Route>
+          <Route exact path="/login" element={<LoginScreen />} />
+          <Route exact path="/register" element={<RegisterScreen />} />
+          <Route exact path="/reset" element={<ResetScreen />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,13 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import LoginScreen from "../screens/auth/LoginScreen";
+import { AuthProvider } from "../contexts/AuthContext";
 
 const PrivateRoute = () => {
-  let logged = useAuth();
-
-  useEffect(() => {
-    console.log("At Private Route: " + logged);
-  });
+  let { logged } = useAuth();
 
   return logged ? <Outlet /> : <LoginScreen />;
 };
